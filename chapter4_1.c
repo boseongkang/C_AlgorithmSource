47f279cffa7b88403733fc275955336dbddf3414
Boseong Kang <okboseong@gmail.com>
2019-12-13 23:13:28 +0900

    chapter4_1

---
+++ chapter4_1.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main() {
+//	int a = 1, b = 1;
+//	float result = 0;
+//	for (a = 1; a <= 9; a++) {
+//		b++;
+//		result += (float)a / (float)b;
+//		printf("%d / %d\n", a,b);
+//	}
+//	printf("%.2f\n", result);
+//}
