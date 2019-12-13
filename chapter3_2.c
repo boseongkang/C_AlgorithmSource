50922c40b0288486641343712cb0880dfb3537eb
Boseong Kang <okboseong@gmail.com>
2019-12-13 23:13:21 +0900

    chapter3_2

---
+++ chapter3_2.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//
+//int main() {
+//	int a = 1, b = 0, cnt = 0;
+//	for (int i = 0; i < 10; i++) {
+//		a += i;
+//		cnt++;
+//		if (cnt % 2 == 0) {
+//			b = b - a;
+//		}
+//		else {
+//			b = b + a;
+//		}
+//	}
+//	printf("%d", b);
+//
+//}
