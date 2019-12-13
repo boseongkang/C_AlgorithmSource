ce9e68545b4d2de382456bda79b773fb4f613cd6
Boseong Kang <okboseong@gmail.com>
2019-12-13 23:13:34 +0900

    chapter4_2

---
+++ chapter4_2.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//
+//int main() {
+//	int a, b = 1;
+//	float sum = 1;
+//	printf("1 ");
+//	for (a = 1; a <= 10; a++) {
+//		sum += 1 / (float)a;
+//		printf("+ 1/%d", a);
+//
+//	}
+//	printf("%.2f\n", sum);
+//}
