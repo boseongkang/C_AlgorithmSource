fab62f05c7effa5c2179bee5e86454edda639784
Boseong Kang <okboseong@gmail.com>
2019-12-18 01:17:20 +0900

    chapter12_1

---
+++ chapter12_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int b[10], s = 0, d, quot = 0, remain ;
+
+	scanf_s("%d", &d);
+	quot = (int)(d/2);
+	do {
+		remain = d - quot*2;
+		printf("³ª¸ÓÁö %d,¸ò %d \n", remain, quot);
+		s++;
+		b[s] = remain;
+		d = quot;
+		quot = (int)(d / 2);
+	} while (quot != 0);
+	remain = d - quot * 2;
+	printf("³ª¸ÓÁö %d, ¸ò %d /\n\n", remain, quot);
+	s++;
+	b[s] = remain;
+
+	for (int i = s; i>0; i--) {
+		printf("%d ", b[i]);
+		puts("");
+	}
+}
