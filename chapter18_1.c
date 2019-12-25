44db8599277b5928a30642ae18f3524026f78253
Boseong Kang <okboseong@gmail.com>
2019-12-26 02:47:52 +0900

    기본행렬1

---
+++ chapter18_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+	int a[5][5], i, j, k = 0;
+
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5; j++) {
+			k++;
+			a[i][j]=k;
+		}
+	}
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5; j++) {
+			if (a[i][j] < 10) {
+				printf(" %d ", a[i][j]);
+			}
+			else {
+				printf("%d", a[i][j]);
+			}
+			puts("");
+		}
+	}
+}
