c2780b1cf7cab2fab25385c10499baaf0bdc2b3b
Boseong Kang <okboseong@gmail.com>
2019-12-29 00:57:32 +0900

    배열 짝수와 홀수 갯수

---
+++ advanced9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int array[11] = {0, 5, 7, 3, 9, 1, 8, 3, 8, 11, 4};
+	int h = 1, rest, even = 0, odd = 0;
+	do {
+		rest = array[h] % 2;
+		if (rest == 0) {
+			even += 1;
+
+		}
+		else {
+			odd += 1;
+		}
+		h++;
+	} while (h < 11);
+	printf("礎熱曖 偎熱 : %d, �汝鶺� 偎熱 : %d\n", even, odd);
+
+}
